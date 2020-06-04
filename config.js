af28b3f1493519972e119dd7e4f996be57f2f5c7
Bregje Brugman <36854179+BregjeBrugman@users.noreply.github.com>
2020-06-04 15:20:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,36 @@
 var respecConfig = {
   specStatus: "GN-WV",
   specType: "IM",
+  subtitle: "Toegepast geologische boormonsterbeschrijving",
   //canonicalURI: "WV",
   //publishDate: "2019-03-25",
   maxTocLevel: 4,
+  logos: [
+  {src: 'https://docs.geostandaarden.nl/bro/gen/logo_bro.png',
+    href: "http://basisregistratieondergrond.nl",
+    alt: "Basisregistratie Ondergrond",
+    width: 252,
+    height: 130,
+    id: 'BRO'},
+  {
+    src: 'https://docs.geostandaarden.nl/bro/gen/logo_minbzk.png',
+    href: "https://www.geobasisregistraties.nl/basisregistraties/ondergrond",
+    alt: "Ministerie BZK",
+    width: 229,
+    height: 130,
+    id: 'BZK',
+  }]  ,	
   editors: [
-    {
-      name: "TNO Geologische Dienst Nederland",
-      url: "https://www.tno.nl/nl/",
-    },
     {
       name: "Geonovum",
       url: "https://www.geonovum.nl", 
     },  
   ],
+  authors: [
+    {
+      name: "Geonovum",
+	  url: "https://www.geonovum.nl",
+	}],
   shortName: "bhr-ag",
   pubDomain: "bro",
   github: "https://github.com/BROprogramma/BHR-AG",
